aaf7a41c4f7ca5f90e59fa5d80d0ccf2fabd8204
Jakub Mazur <153779115+Aspoleczny777@users.noreply.github.com>
2025-11-15 13:45:43 +0100

    Update Portainer start command and ports

--- docker-install.sh
+++ docker-install.sh
@@ -38,9 +38,10 @@ docker info -f '{{ .DockerRootDir}}'
 docker volume create portainer_data
 
 # Create container with Portainera
-docker run -d -p 8001:8000 -p 5000:9443 --name portainer --restart=always \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  -v portainer_data:/data portainer/portainer-ce:lts
+docker stop portainer
+docker rm portainer
+
+docker run -d -p 9000:9000 -p 5000:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 
 # Final check
 docker container list
