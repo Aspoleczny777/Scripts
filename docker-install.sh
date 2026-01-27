a913c6fb3a92ac6aca2986179f395b478dd3fc8e
Jakub Mazur <153779115+Aspoleczny777@users.noreply.github.com>
2026-01-27 14:40:25 +0100

    Remove Portainer setup from docker-install script
    Removed Portainer container setup and related commands.

--- docker-install.sh
+++ docker-install.sh
@@ -33,15 +33,3 @@ systemctl start docker
 
 # Check home dir
 docker info -f '{{ .DockerRootDir}}'
-
-# Create portainer dir 
-docker volume create portainer_data
-
-# Create container with Portainera
-docker stop portainer
-docker rm portainer
-
-docker run -d -p 9000:9000 -p 5000:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
-
-# Final check
-docker container list
